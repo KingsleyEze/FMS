f9dd10ad22dee7efa85ebae8c6d710b0fc158c70
ikingsley <kingsley.mx@gmail.com>
2017-11-09 12:23:59 +0100

    updated view model and forms

--- FMS/Models/Account/UserDetailView.cs
+++ FMS/Models/Account/UserDetailView.cs
@@ -7,5 +7,19 @@ namespace FMS.Models.Account
 {
     public class UserDetailView
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string LGA { get; set; }
+        public string Phone { get; set; }
+        public string EmailAddress { get; set; }
+        public string Fax { get; set; }
+        public string PostalAddress { get; set; }
+        public string Website { get; set; }
+        public string FileId { get; set; }
     }
 }
