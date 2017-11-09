9857f43d1107b8c7ef7c3aea0f5bb8fe96d37948
ikingsley <kingsley.mx@gmail.com>
2017-11-09 10:11:31 +0100

    added search and result page for payment

---
+++ FMS/Models/Payment/SearchPaymentView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FMS.Core.Model;
+
+namespace FMS.Models.Payment
+{
+    public class SearchPaymentView
+    {
+    
+        public string PayerId { get; set; }
+        public string BillNumber { get; set; }
+        public string Amount { get; set; }
+        public string TransactionDate { get; set; }
+        public dynamic SearchResult { get; set; }
+
+    
+    }
+}
